c01283a80f5eb7524513a20b2c3b0e35af758e0e
Kate-Sagittarius <kate.zakhovaiko@gmail.com>
2019-06-07 01:33:39 +0300

    refactor html builder

--- js/main.js
+++ js/main.js
@@ -1,81 +1,60 @@
+const MAIN = document.getElementById('main-section');
+
+const CREATE_ELEMENT = (elementName, className, parent) => {
+  const tag = document.createElement(elementName);
+  tag.setAttribute("class", className);
+  parent.appendChild(tag);
+
+  return tag;
+};
+
+const CREATE_CARD = (word, authorName, definitions, examples, likes, i) => {
+  const card = CREATE_ELEMENT('div', 'main-section__card', MAIN);
+  const cardWord = CREATE_ELEMENT('div', 'main-section__card__word', card);
+  const cardNumber = CREATE_ELEMENT('h3', 'main-section__card__number', cardWord);
+
+  if (i===0) {
+    cardNumber.setAttribute("class", "main-section__card__number main-section__card__number--top");
+    cardNumber.innerHTML = 'Top definition';
+  } else {
+    cardNumber.innerHTML = i;
+  }
+
+  const wordName = CREATE_ELEMENT('h2', 'main-section__card__word-name', cardWord);
+  const author = CREATE_ELEMENT('h4', 'main-section__card__author', card);
+  const cardDefinition = CREATE_ELEMENT('div', 'main-section__card__text', card);
+  const cardDefinitionHeading = CREATE_ELEMENT('strong', 'main-section__card__text--strong', cardDefinition);
+  const cardDefinitionText = CREATE_ELEMENT('p', 'main-section__card__text__definition', cardDefinition);
+  const cardExample = CREATE_ELEMENT('div', 'main-section__card__text', card);
+  const cardExampleHeading = CREATE_ELEMENT('strong', 'main-section__card__text--strong', cardExample);
+  const cardExampconstext = CREATE_ELEMENT('p', 'main-section__card__text__example', cardExample);
+  const cardLikes = CREATE_ELEMENT('div', 'main-section__card__likes', card);
+  const likeImg = CREATE_ELEMENT('img', 'like-img', cardLikes);
+  const likeNums = CREATE_ELEMENT('p', 'number number-of-likes', cardLikes);
+
+  likeImg.setAttribute("src", "images/heart.svg");
+  likeImg.setAttribute("alt", "Like");
+
+  wordName.innerHTML = word;
+  author.innerHTML = authorName;
+  cardDefinitionHeading.innerHTML = 'Definitions';
+  cardDefinitionText.innerHTML = definitions.replace(/[\[\]]/g, '');
+  cardExampleHeading.innerHTML = 'Examples';
+  cardExampconstext.innerHTML = examples.replace(/[\[\]]/g, '');
+  likeNums.innerHTML = likes;
+
+  likeImg.onclick = () => {
+    likeNums.innerHTML = parseInt(likeNums.innerHTML, 10) + 1;
+  };
+}
+
 document.getElementById('search-button').onclick = () => {
-  let word = document.getElementById('search-field').value;
+  const word = document.getElementById('search-field').value;
   axios.get(`http://api.urbandictionary.com/v0/define?term=${word}`)
-      .then(response => {
-        console.log(response);
-
-        let main = document.getElementById('main-section');
-        main.innerHTML = '';
-
-        response.data.list.forEach((x, i) => {
-          console.log(x);
-          let card = document.createElement('div');
-          card.setAttribute("class", "main-section__card");
-          main.appendChild(card);
-
-          let cardWord = document.createElement('div');
-          cardWord.setAttribute("class", "main-section__card__word");
-          card.appendChild(cardWord);
-
-          let cardNumber = document.createElement('h3');
-          if (i == 0) {
-            cardNumber.innerHTML = 'Top definition';
-            cardNumber.setAttribute("class", "main-section__card__number main-section__card__number--top")
-          } else {
-            cardNumber.setAttribute("class", "main-section__card__number");
-            cardNumber.innerHTML = i;
-          }
-          cardWord.appendChild(cardNumber);
-
-          let wordName = document.createElement('h2');
-          wordName.setAttribute("class", "main-section__card__word-name");
-          wordName.innerHTML = x.word;
-          cardWord.appendChild(wordName);
-
-          let author = document.createElement('h4');
-          author.setAttribute("class", "main-section__card__author");
-          author.innerHTML = x.author;
-          card.appendChild(author);
-
-          let cardDefinition = document.createElement('div');
-          cardDefinition.setAttribute("class", "main-section__card__text");
-          card.appendChild(cardDefinition);
-          let cardDefinitionHeading = document.createElement('strong');
-          cardDefinitionHeading.innerHTML = 'Definitions';
-          cardDefinition.appendChild(cardDefinitionHeading);
-          let cardDefinitionText = document.createElement('p');
-          cardDefinitionText.innerHTML = x.definition.replace(/[\[\]]/g, '');
-          cardDefinition.appendChild(cardDefinitionText);
-
-          let cardExample = document.createElement('div');
-          cardExample.setAttribute("class", "main-section__card__text");
-          card.appendChild(cardExample);
-          let cardExampleHeading = document.createElement('strong');
-          cardExampleHeading.innerHTML = 'Examples';
-          cardExample.appendChild(cardExampleHeading);
-          let cardExampleText = document.createElement('p');
-          cardExampleText.innerHTML = x.example.replace(/[\[\]]/g, '');
-          cardExample.appendChild(cardExampleText);
-
-          let cardLikes = document.createElement('div');
-          cardLikes.setAttribute("class", "main-section__card__likes");
-          card.appendChild(cardLikes);
-          let likeImg = document.createElement('img');
-          likeImg.setAttribute("class", "like-img");
-          likeImg.setAttribute("src", "images/heart.svg");
-          likeImg.setAttribute("alt", "Like");
-          cardLikes.appendChild(likeImg);
-          let likeNums = document.createElement('p');
-          likeNums.setAttribute("class", "number number-of-likes");
-          likeNums.innerHTML = x.thumbs_up;
-          cardLikes.appendChild(likeNums);
-
-          likeImg.onclick = () => {
-            likeNums.innerHTML = parseInt(likeNums.innerHTML, 10) + 1;
-          };
-
-        })
-
-
+    .then(response => {
+      MAIN.innerHTML = '';
+      response.data.list.forEach((x, i) => {
+        CREATE_CARD(x.word, x.author, x.definition, x.example, x.thumbs_up, i);
+      })
   })
 }
